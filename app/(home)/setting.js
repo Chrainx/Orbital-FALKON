7dd344c954942a2e819fcc0ff11fc1104cfcea32
jovkusuma <jovankusuma1@gmail.com>
2023-06-09 23:50:29 +0800

    setting icons

--- app/(home)/setting.js
+++ app/(home)/setting.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, Text} from 'react-native';
+import { SafeAreaView, View, Text, Image, } from 'react-native';
 import { NewItem } from './newItem';
 import { TextInput, Button } from 'react-native-paper';
 import { useAuth } from '../../contexts/auth';
@@ -8,6 +8,8 @@ export default function report () {
   const {user} = useAuth();
   const [newCategory, setNewCategory] = useState('');
   const [CategoryDetail, setCategoryDetail] = useState(false);
+  const [newCurrency, setNewCurrency] = useState('');
+  const [CurrencyDetail, setCurrencyDetail] = useState(false);
 
   return (
     <SafeAreaView>
@@ -17,7 +19,7 @@ export default function report () {
           icon = { 
             CategoryDetail
             ? <Text style={{color: 'red'}}> Cancel </Text> 
-            : <Text style ={{color: 'white'}}> Add/bisa ganti ke gambar pake Image </Text>
+            : <Image source={require('./tab-icons/addCategory.png')} resizeMode="contain" style={{ width: 25, height: 25, }}/>
           }
           detail = {
             CategoryDetail 
@@ -50,16 +52,51 @@ export default function report () {
         />
         <NewItem 
           title= 'Currency Type' 
-          icon={<Text style={{color: 'white'}}> Gambar Currency </Text>}
+          icon={CurrencyDetail
+            ? <Text style={{color: 'red'}}> Cancel </Text> 
+            : <Image source={require('./tab-icons/currency.png')} resizeMode="contain" style={{ width: 25, height: 25, }}/>
+          }
+          detail = {
+            CurrencyDetail 
+            ? <View style = {{flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent:'space-between', backgroundColor:'black'}}>
+              <TextInput
+                placeholder= 'Mau dibikin drop-down?'
+                value={newCurrency} 
+                onChangeText={setNewCurrency}
+                style={{ 
+                  height:50,
+                  color: 'white',
+                  backgroundColor:'black',
+                  marginLeft: 10,
+                  width: '82%'
+                }}
+              /> 
+              <Button onPress={() => undefined}> + </Button> 
+            </View>
+            : undefined}
+          action = {() => {
+            if (CurrencyDetail) {
+              setCurrencyDetail(false);
+            } else {
+              setCurrencyDetail(true)}
+            }
+          }
         />
         <NewItem 
           title= 'Erase all data' 
           isDestructive 
-          icon={<Text style={{color: 'white'}}>  Gambar tongsampah untuk delete</Text>} 
+          icon={CategoryDetail
+            ? <Text style={{color: 'red'}}> Cancel </Text> 
+            : <Image source={require('./tab-icons/trashcan.png')} resizeMode="contain" style={{ width: 25, height: 25, }}/>
+          }
+        
         />
         <NewItem 
           title= 'Logout' 
-          icon= {<Text style={{color: 'white'}}> Gambar Logout </Text>}
+          icon= {CategoryDetail
+            ? <Text style={{color: 'red'}}> Cancel </Text> 
+            : <Image source={require('./tab-icons/logout.png')} resizeMode="contain" style={{ width: 20, height: 20, }}/>
+          }
         />
     </SafeAreaView>
   );
