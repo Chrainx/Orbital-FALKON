7b776651d180c5928c6e1211778b10076d24ea5b
Fredy Lawrence <fredylawrence2003@gmail.com>
2023-06-17 13:46:30 +0700

    category

--- app/(home)/category.js
+++ app/(home)/category.js
@@ -1,9 +1,26 @@
-import { SafeAreaView, View, Text, TextInput } from 'react-native';
+import { SafeAreaView, View, Text, ScrollView} from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { useState, useEffect } from 'react';
 
 export default function Category() {
+  const [newCategory, setNewCategory] = useState('');
   return (
-    <SafeAreaView>
-      <Text> category </Text>
-    </SafeAreaView>
+    <View style = {{flex: 1}}>
+      <ScrollView
+        style = {{flex: 1}}
+
+      >
+
+      </ScrollView>
+      <View style = {{flexDirection: 'row', width: '100%', justifyContent: 'space-around', alignItems: 'center'}}>
+        <Text> color </Text>
+        <TextInput
+          style = {{justifyContent: 'flex-end'}}
+          placeholder='Insert a category'
+          onChangeText={setNewCategory}
+        />
+        <Text> ADD </Text>
+      </View>
+    </View>
   );
 }
--- app/(home)/setting.js
+++ app/(home)/setting.js
@@ -1,54 +1,19 @@
 import { SafeAreaView, View, Text, Image, Alert } from 'react-native';
 import { NewSetting } from './newSetting';
-import { TextInput, Button, ActivityIndicator } from 'react-native-paper';
+import { TextInput, Button } from 'react-native-paper';
 import { useAuth } from '../../contexts/auth';
 import { useState } from 'react';
 import { supabase } from '../../lib/supabase';
 import { useNavigation } from '@react-navigation/native';
-import Category from './category';
 
 
 // Use Modal to choose Category
 export default function Setting () {
   const {user} = useAuth();
-  const [loading, setLoading] = useState(false);
-  const [errCategoryMsg, setErrCategoryMsg] = useState('');
-  const [newCategory, setNewCategory] = useState('');
-  const [CategoryDetail, setCategoryDetail] = useState(false);
   const [newCurrency, setNewCurrency] = useState('');
   const [CurrencyDetail, setCurrencyDetail] = useState(false);
   const navigation = useNavigation();
 
-  const handleAdd = async () => {
-    setErrCategoryMsg('');
-    setNewCategory('');
-    if (newCategory == '') {
-      setErrCategoryMsg("New category cannot be empty");
-      setCategoryDetail(false);
-      return;
-    }
-    setLoading(true);
-    const { data, count, error } = await supabase.from('category').select('*', {count: 'exact'}).eq("user_id", user.id).eq("category", newCategory);
-    if (count != 0) {
-      setErrCategoryMsg(newCategory + " already exist");
-      setLoading(false);
-      setCategoryDetail(false);
-      return;
-    } else {
-      const { error } = await supabase.from('category').
-      insert({inserted_at: new Date(), category: newCategory, user_id: user.id})
-      .select();
-    }
-    setLoading(false);
-    if (error != null) {
-      setErrCategoryMsg(error.message);
-      setCategoryDetail(false);
-      return;
-    }
-    if (errCategoryMsg == '') {
-      setCategoryDetail(false);
-    }
-  }
   
   const alertDelete = () => {
       Alert.alert(
@@ -87,38 +52,15 @@ export default function Setting () {
         <Text> Settings </Text>
         <NewSetting
           title = 'Add Category'
-          icon = { 
-            CategoryDetail
-            ? <Text style={{color: 'red'}}> Cancel </Text> 
-            : <Image source={require('./tab-icons/addCategory.png')} resizeMode="contain" style={{ width: 25, height: 25, }}/>
+          icon = {<Image source={require('./tab-icons/addCategory.png')} resizeMode="contain" style={{ width: 25, height: 25, }}/>
           }
-          detail = {
-            CategoryDetail 
-            ? <View style = {{flexDirection: 'row', alignItems: 'center', width: '100%', justifyContent:'space-between', backgroundColor:'black'}}>
-              <TextInput
-                placeholder= 'Insert A New Category'
-                value={newCategory} 
-                onChangeText={setNewCategory}
-                style={{ 
-                  height:50,
-                  color: 'white',
-                  backgroundColor:'black',
-                  marginLeft: 10,
-                  width: '82%'
-                }}
-              /> 
-              <Button onPress={handleAdd}> + </Button> 
-            </View>
-            : undefined}
           action = {
             () => {
             navigation.navigate("Category");
-            //setErrCategoryMsg('');
-            //setCategoryDetail(!CategoryDetail);
+
             }
           }
         />
-        {errCategoryMsg !== '' && <Text style={{color:'red'}}> {errCategoryMsg}! </Text>}
         <NewSetting 
           title= 'Currency Type' 
           icon={CurrencyDetail
@@ -172,7 +114,6 @@ export default function Setting () {
           icon= {<Image source={require('./tab-icons/logout.png')} resizeMode="contain" style={{ width: 20, height: 20, }}/>}
           action= {alertLogout} 
         />
-         {loading && <ActivityIndicator />}
     </SafeAreaView>
   );
 }
